73202ea4b2e7605d8f8f23399d9ab5eb51d7e66b
hyeongju you <61458960+hyeongju12@users.noreply.github.com>
2021-12-18 12:21:57 +0900

    category 생성 기능 추가

--- Todolist/RestaurantShare/shareRes/views.py
+++ Todolist/RestaurantShare/shareRes/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from shareRes.models import *
 # Create your views here.
 
 def index(request):
     # return HttpResponse('index')
-    return render(request, 'shareRes/index_init.html')
+    categories = Category.objects.all()
+    content = {'categories' : categories}
+    return render(request, 'shareRes/index_init.html', content)
 
 def restaurantDetail(request):
     # return HttpResponse('restaurantDetail')
@@ -16,4 +20,13 @@ def restaurantCreate(request):
 
 def categoryCreate(request):
     # return HttpResponse('categoryCreate')
-    return render(request, 'shareRes/categoryCreate_init.html')
+    categories = Category.objects.all()
+    content = {'categories' : categories}
+    return render(request, 'shareRes/categoryCreate_init.html', content)
+
+def Create_category(request):
+    category_name = request.POST['categoryName']
+    new_category = Category(category_name = category_name)
+    new_category.save()
+    return HttpResponseRedirect(reverse('index'))
+    #return HttpResponse("여기서 카테고리 기능 구현")
