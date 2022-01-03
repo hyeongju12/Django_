675209f34146193a29c075ca8a6e837e43df13f5
hyeongju you <61458960+hyeongju12@users.noreply.github.com>
2022-01-03 22:50:54 +0900

    수정

--- Todolist/RestaurantShare/shareRes/urls.py
+++ Todolist/RestaurantShare/shareRes/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('restaurantCreate/', views.restaurantCreate, name='resCreatePage'),
     path('categoryCreate/', views.categoryCreate, name='cateCreatePage'),
     path('categoryCreate/create', views.Create_category, name='cateCreate'),
-    path('categoryCreate/delete', views.Delete_category, name="catedelete")
+    path('categoryCreate/delete', views.Delete_category, name="cateDelete")
 ]
