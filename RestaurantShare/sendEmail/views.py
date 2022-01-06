da47b130ec534ed549f23b7d33946dcb1c1c5f65
hyeongju you <61458960+hyeongju12@users.noreply.github.com>
2022-01-07 00:20:15 +0900

    이메일 전송 실패/성공 페이지 추가

--- RestaurantShare/sendEmail/views.py
+++ RestaurantShare/sendEmail/views.py
@@ -10,23 +10,26 @@ from django.template.loader import render_to_string
 # Create your views here.
 
 def sendEmail(request):
-    check_res_list = request.POST.getlist('checks')
-    inputReceiver = request.POST['inputReceiver']
-    inputTitle = request.POST['inputTitle']
-    inputContent = request.POST['inputContent']
-    restaurants = []
-    for checked_res_id in check_res_list:
-        restaurants.append(Restaurant.objects.get(id=checked_res_id))
-    content = {'inputContent' : inputContent, 'restaurant' : restaurants}
-    # smtp
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.login("hyeongju12@gmail.com", "")
+    try:
+        check_res_list = request.POST.getlist('checks')
+        inputReceiver = request.POST['inputReceiver']
+        inputTitle = request.POST['inputTitle']
+        inputContent = request.POST['inputContent']
+        restaurants = []
+        for checked_res_id in check_res_list:
+            restaurants.append(Restaurant.objects.get(id=checked_res_id))
+        content = {'inputContent' : inputContent, 'restaurant' : restaurants}
+        # smtp
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login("hyeongju12@gmail.com", "")
 
-    msg_html = render_to_string('sendEmail/email_format.html', content)
-    msg = EmailMessage(subject=inputTitle, body=msg_html, from_mail="hyeongjnu12@gmail.com", bcc=inputReceiver.split(','))
-    msg.content_subtype = 'html'
-    msg.send()
+        msg_html = render_to_string('sendEmail/email_format.html', content)
+        msg = EmailMessage(subject=inputTitle, body=msg_html, from_mail="hyeongjnu12@gmail.com", bcc=inputReceiver.split(','))
+        msg.content_subtype = 'html'
+        msg.send()
 
-    return HttpResponseRedirect(reverse('index'))
+        return render(request, 'sendEmail/sendSuccess.html')
+    except:
+        return render(request, 'sendEmail/sendFail.html')
 
 
